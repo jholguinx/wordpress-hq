171f560f6c796a28ed842bc1ce75aab9376991c3
Faggioni <miguelfaggioni@gmail.com>
2018-12-07 19:54:08 -0400

    hq

--- includes/tasks/HQRentalsJob.php
+++ includes/tasks/HQRentalsJob.php
@@ -3,5 +3,23 @@
 namespace HQRentalsPlugin\HQRentalsTasks;
 use HQRentalsPlugin\HQRentalsTasks\HQRentalsScheduler;
 
-$scheduler = new HQRentalsScheduler();
-$scheduler->refreshHQData();
+class HQRentalsJob{
+    public function __construct()
+    {
+        $scheduler = new HQRentalsScheduler();
+        add_action( 'refreshAllHQDataJob', array( $this, 'refreshAllData' );
+        if ( ! wp_next_scheduled( array( $this, 'refreshAllData' ) ) ) {
+            wp_schedule_event( time(), 'hourly', 'refreshAllHQDataJob' );
+        }
+    }
+    function refreshAllData(){
+
+    }
+    function caag_hq_vehicle_classes_cron_job()
+    {
+    }
+    add_action('caag_hq_vehicle_classes_update','caag_hq_vehicle_classes_cron_job');
+
+}
+
+

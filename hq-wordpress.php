c15914572b44882e56c4efaaf230edb2fe07a287
nadin <nadin@caagcrm.com>
2019-01-26 14:20:37 -0400

    Updated author

--- hq-wordpress.php
+++ hq-wordpress.php
@@ -4,26 +4,30 @@ Plugin Name: HQ Rental Plugin
 Plugin URI: https://hqrentalsoftware.com
 Description: HQ Rental Software Plugin
 Version: 1.0.0
-Author: Miguel Faggioni
+Author: HQ Rental Software
 Author URI: https://hqrentalsoftware.com
 Text Domain: hq-wordpress
 */
+
 namespace HQRentalsPlugin;
-require_once('includes/autoloader.php');
+
+require_once( 'includes/autoloader.php' );
 // If this file is accessed directory, then abort.
 if ( ! defined( 'WPINC' ) ) {
-    die;
+	die;
 }
+
 use HQRentalsPlugin\HQRentalsSettings\HQRentalsBootstrap;
 use HQRentalsPlugin\HQRentalsBootstrap\HQRentalsBootstrapPlugin;
+
 $bootstraper = new HQRentalsBootstrapPlugin();
 /*
  * Activation Routine
  * @return void
  */
-function hq_rentals_wordpress_activation()
-{
-    $boot = new HQRentalsBootstrap();
-    $boot->onPluginActivation();
+function hq_rentals_wordpress_activation() {
+	$boot = new HQRentalsBootstrap();
+	$boot->onPluginActivation();
 }
-register_activation_hook(__FILE__,__NAMESPACE__ . '\hq_rentals_wordpress_activation');
+
+register_activation_hook( __FILE__, __NAMESPACE__ . '\hq_rentals_wordpress_activation' );

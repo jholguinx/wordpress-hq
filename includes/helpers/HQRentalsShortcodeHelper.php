ec7a6d6cdcf12b159f78c051e7ca8137f8d3fb31
Faggioni <miguelfaggioni@gmail.com>
2019-05-30 09:56:46 -0400

    add support for safari step 2

--- includes/helpers/HQRentalsShortcodeHelper.php
+++ includes/helpers/HQRentalsShortcodeHelper.php
@@ -2,13 +2,20 @@
 
 namespace HQRentalsPlugin\HQRentalsHelpers;
 
+use HQRentalsPlugin\HQRentalsSettings\HQRentalsSettings;
+use HQRentalsPlugin\HQRentalsVendor\Carbon;
+
 class HQRentalsShortcodeHelper
 {
-
+    public function __construct()
+    {
+        $this->settings = new HQRentalsSettings();
+        $this->dateHelper = new HQRentalsDatesHelper();
+    }
 
     public function resolvesSafariIssue($is_safari, $post_data, $iframeBaseUrl)
     {
-        if ($is_safari and $this->noDNSConfig(esc_url($iframeBaseUrl))) {
+        if ($is_safari and $this->noDNSConfig($iframeBaseUrl)) {
             $this->setForSafariBrowser($post_data, esc_url($iframeBaseUrl));
         } else {
             $this->setForNotSafariBrowser($post_data, esc_url($iframeBaseUrl));
@@ -23,10 +30,26 @@ class HQRentalsShortcodeHelper
                 'isSafari' => 1
             );
         } else {
-            $metaDataSafari = array(
-                'urlRedirect' => $urlFirstStep . '&' . http_build_query($postData) . '&target_step=2',
-                'isSafari' => 1
-            );
+            try {
+                if ($postData['pick_up_date']) {
+                    if ($postData['pick_up_time']) {
+                        $pickup_date = Carbon::createFromFormat($this->settings->getFrontEndDatetimeFormat(), $postData['pick_up_date'] . ' ' . $postData['pick_up_time']);
+                        $return_date = Carbon::createFromFormat($this->settings->getFrontEndDatetimeFormat(), $postData['return_date'] . ' ' . $postData['return_time']);
+                    } else {
+                        $pickup_date = Carbon::createFromFormat($this->settings->getFrontEndDatetimeFormat(), $postData['pick_up_date']);
+                        $return_date = Carbon::createFromFormat($this->settings->getFrontEndDatetimeFormat(), $postData['return_date']);
+                    }
+                    $metaDataSafari = array(
+                        'urlRedirect' => $urlFirstStep. $this->getQueryStringForStep2OnSafari($postData, $pickup_date,$return_date),
+                        'isSafari' => 1
+                    );
+                }
+            } catch (Exception $e) {
+                $metaDataSafari = array(
+                    'urlRedirect' => $urlFirstStep,
+                    'isSafari' => 1
+                );    
+            }
         }
         wp_localize_script('hq-resize-script', 'hqSafariData', $metaDataSafari);
     }
@@ -44,5 +67,19 @@ class HQRentalsShortcodeHelper
     {
         return (strpos($urlFirstStep, 'caagcrm') !== false) or
             (strpos($urlFirstStep, 'hqrentals') !== false);
+    }
+
+    public function getQueryStringForStep2OnSafari($post_data, $pickUpDate, $returnDate)
+    {
+        $queryString = '';
+        foreach($post_data as $key => $value){
+            if(($key !== 'pick_up_date' and $key !== 'pick_up_time' and $key !== 'return_date' and $key !== 'return_time')){
+                $queryString .= '&' . $key . '=' . $value;
+            }
+        }
+        $queryString .= '&pick_up_date=' . $pickUpDate->format($this->dateHelper->getDateFormatFromCarbon($this->settings->getHQDatetimeFormat())) . ' ' . $pickUpDate->format($this->dateHelper->getTimeFormatFromCarbon($this->settings->getHQDatetimeFormat()));
+        $queryString .= '&return_date=' . $returnDate->format($this->dateHelper->getDateFormatFromCarbon($this->settings->getHQDatetimeFormat())) . ' ' . $returnDate->format($this->dateHelper->getTimeFormatFromCarbon($this->settings->getHQDatetimeFormat()));
+        $queryString .= '&target_step=2';
+        return $queryString;
     }
 }

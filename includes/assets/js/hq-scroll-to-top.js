5a536c1a685ac511920656b3ae9add6fb191be85
Faggioni <miguelfaggioni@gmail.com>
2019-06-21 13:10:01 -0400

    fix ECMA syntax

--- includes/assets/js/hq-scroll-to-top.js
+++ includes/assets/js/hq-scroll-to-top.js
@@ -5,7 +5,7 @@
 var eventMethod = window.addEventListener ? "addEventListener" : "attachEvent";
 var eventer = window[eventMethod];
 var messageEvent = eventMethod === "attachEvent" ? "onmessage" : "message";
-let firstTime = true;
+var firstTime = true;
 eventer(messageEvent, function (e) {
     if (e.data === 'hq-scroll-to-top' || e.message === "hq-scroll-to-top") {
         if(firstTime){
@@ -14,16 +14,14 @@ eventer(messageEvent, function (e) {
             window.scrollTo( 0,findPos(document.getElementById("hq-rental-iframe")));
         }
     }
-}
-)
+});
 //Finds y value of given object
 function findPos(obj) {
     var curtop = 0;
-    
     if (obj.offsetParent) {
         do {
             curtop += obj.offsetTop;
         } while (obj = obj.offsetParent);
     return [curtop];
     }
-};
+}

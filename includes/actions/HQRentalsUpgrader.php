342a0caaef8a9040ba6e0f60acd5d93abcb7b5a6
Miguel Faggioni <miguelfaggioni@gmail.com>
2019-09-25 18:11:11 -0400

    updater fixes

--- includes/actions/HQRentalsUpgrader.php
+++ includes/actions/HQRentalsUpgrader.php
@@ -2,6 +2,7 @@
 
 namespace HQRentalsPlugin\HQRentalsActions;
 
+
 require_once ABSPATH . 'wp-admin/includes/class-wp-upgrader.php';
 require_once ABSPATH . 'wp-admin/includes/misc.php';
 require_once ABSPATH . 'wp-admin/includes/class-wp-upgrader-skin.php';
@@ -11,10 +12,11 @@ class HQRentalsUpgrader{
 
     public function __construct()
     {
-        $this->upgrader = new Plugin_Upgrader();
+
+        $this->upgrader = new \Plugin_Upgrader();
     }
     public function upgradePlugin()
     {
-        $this->upgrader->upgrade('hq-rental-software');
+        $this->upgrader->upgrade('hq-wordpress');
     }
 }

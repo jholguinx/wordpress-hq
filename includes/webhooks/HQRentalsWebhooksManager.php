57a7e64b7d75bb0c5eb14f7b739a2be84f08a3d5
Miguel Faggioni <miguelfaggioni@gmail.com>
2019-09-25 18:06:32 -0400

    wordpress upgrader

--- includes/webhooks/HQRentalsWebhooksManager.php
+++ includes/webhooks/HQRentalsWebhooksManager.php
@@ -2,31 +2,50 @@
 
 namespace HQRentalsPlugin\Webhooks;
 use HQRentalsPlugin\HQRentalsTasks\HQRentalsScheduler;
+use HQRentalsPlugin\HQRentalsActions\HQRentalsUpgrader;
 
 class HQRentalsWebhooksManager{
 
     public function __construct()
     {
         $this->scheduler = new HQRentalsScheduler();
+        $this->upgrader = new HQRentalsUpgrader();
         add_action( 'rest_api_init', array($this, 'setCustomAPIRoutes') );
     }
     public function setCustomApiRoutes(){
+        //baseURl/wp-json/hqrentals/update/
         register_rest_route( 'hqrentals', '/update/', array(
             'methods' => 'POST',
             'callback' => array ($this, 'fireUpdate'),
+        ) );
+        //baseURl/wp-json/hqrentals/plugin/upgrade/
+        register_rest_route( 'hqrentals', '/plugin/upgrade/', array(
+            'methods' => 'POST',
+            'callback' => array ($this, 'firePluginUpgrade'),
         ) );
     }
     public function fireUpdate(\WP_REST_Request $request)
     {
         //Should be validated -> add Success params to the response
         $this->scheduler->refreshHQData();
-        $data = array(
+        $data = $this->resolveResponse();
+        $response = new \WP_REST_Response($data);
+        $response->status = 200;
+        return $response;
+    }
+    public function firePluginUpgrade()
+    {
+        $this->upgrader->upgradePlugin();
+        $data = $this->resolveResponse();
+        $response = new \WP_REST_Response($data);
+        $response->status = 200;
+        return $response;
+    }
+    public function resolveResponse(){
+        return array(
             'message'   =>  'Successful Update',
             'status'    =>  200,
             'data'      =>  [ ]
         );
-        $response = new \WP_REST_Response($data);
-        $response->status = 200;
-        return $response;
     }
 }

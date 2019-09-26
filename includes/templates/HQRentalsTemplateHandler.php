2eed97d833a03c27b7a941ab00c4767b3ddabdd8
Miguel Faggioni <miguelfaggioni@gmail.com>
2019-09-25 20:31:28 -0400

    adding support for temapltes

--- includes/templates/HQRentalsTemplateHandler.php
+++ includes/templates/HQRentalsTemplateHandler.php
@@ -5,13 +5,12 @@ namespace HQRentalsPlugin\HQRentalsTemplates;
 class HQRentalsTemplateHandler{
     public function __construct()
     {
-        add_filter( 'template_include', array($this, 'addingTemplates') );
+        add_filter( 'template_include', array( $this, 'addingTemplates') );
     }
-    public function addingTemplates($template)
+    public function addingTemplates($defaultTemplate)
     {
         if( is_page('quote') ){
-            return 'page-quote.php';
+            load_template( __DIR__ . '/page-quote.php' );
         }
     }
-
 }
--- includes/templates/page-quote.php
+++ includes/templates/page-quote.php
@@ -1,6 +1,22 @@
 <?php
 get_header();
+$url = 'https://codex.wordpress.org/Plugin_API/Filter_Reference/template_include';
 ?>
-<div>hello world</div>
+    <div class="hq-container">
+        <div class="hq-iframe-wrapper">
+            <?php echo do_shortcode('[hq_rentals_form_link url=' . $url . ']')?>
+        </div>
+    </div>
+    <style>
+        .hq-container{
+            flex:1;
+            display: flex;
+            justify-content: center;
+            align-items: center;
+        }
+        .hq-iframe-wrapper{
+            flex:1;
+        }
+    </style>
 <?php
 get_footer();

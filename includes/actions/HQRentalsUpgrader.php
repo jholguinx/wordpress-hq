8b31e6189b92e4ca4cbf4f5d66ac1b0bab303370
Miguel Faggioni <miguelfaggioni@gmail.com>
2019-11-02 15:28:10 -0400

    support for activation

--- includes/actions/HQRentalsUpgrader.php
+++ includes/actions/HQRentalsUpgrader.php
@@ -8,12 +8,33 @@ require_once ABSPATH . 'wp-admin/includes/class-plugin-upgrader.php';
 require_once ABSPATH . 'wp-admin/includes/file.php';
 
 class HQRentalsUpgrader{
+
+    protected $pluginName = 'hq-rental-software/hq-wordpress.php';
+
     public function __construct()
     {
         $this->upgrader = new \Plugin_Upgrader();
     }
     public function upgradePlugin()
     {
-        return $this->upgrader->upgrade('hq-rental-software/hq-wordpress.php');
+        $result = $this->upgrader->upgrade($this->pluginName);
+        if($result){
+            $this->activatePlugin($this->pluginName);
+        }
+        return $result;
+    }
+    public function activatePlugin( $plugin ) {
+        $current = get_option( 'active_plugins' );
+        $plugin = plugin_basename( trim( $plugin ) );
+        if ( !in_array( $plugin, $current ) ) {
+            $current[] = $plugin;
+            sort( $current );
+            do_action( 'activate_plugin', trim( $plugin ) );
+            update_option( 'active_plugins', $current );
+            do_action( 'activate_' . trim( $plugin ) );
+            do_action( 'activated_plugin', trim( $plugin) );
+        }
+
+        return null;
     }
 }

cde0d6b6c3fbd63e3ad5c03e87c4399f0c80875b
Miguel Faggioni <miguelfaggioni@gmail.com>
2020-04-28 16:10:02 -0400

    message

--- includes/tasks/HQRentalsScheduler.php
+++ includes/tasks/HQRentalsScheduler.php
@@ -54,7 +54,7 @@ class HQRentalsScheduler
                 $_POST['success'] = 'success';
             }else{
                 $error = $this->getErrorOnSync();
-                $error = "There was an issue with your request. Please verify tokens and region.";
+                $error = "There was an issue with your request. Please verify tokens and installation region.";
                 $this->setErrorMessage($error);
             }
         }catch(Exception $e){

c954487e91b0a20ea6c93092c4d6440e8ef9bc23
Miguel Faggioni <miguelfaggioni@gmail.com>
2021-08-16 14:16:33 -0400

    rates table

--- includes/db/HQRentalsDbBootstrapper.php
+++ includes/db/HQRentalsDbBootstrapper.php
@@ -2,6 +2,7 @@
 
 namespace HQRentalsPlugin\HQRentalsDb;
 
+use HQRentalsPlugin\HQRentalsModels\HQRentalsModelsActiveRate;
 use HQRentalsPlugin\HQRentalsModels\HQRentalsModelsBrand;
 use HQRentalsPlugin\HQRentalsModels\HQRentalsModelsLocation;
 use HQRentalsPlugin\HQRentalsModels\HQRentalsModelsVehicleClass;
@@ -15,6 +16,7 @@ class HQRentalsDbBootstrapper
         $this->brandsModel = new HQRentalsModelsBrand();
         $this->locationModel = new HQRentalsModelsLocation();
         $this->vehicleClassModel = new HQRentalsModelsVehicleClass();
+        $this->activeRates = new HQRentalsModelsActiveRate();
     }
 
     public function createTablesOnInit()
@@ -22,11 +24,13 @@ class HQRentalsDbBootstrapper
         $brandData = $this->brandsModel->getDataToCreateTable();
         $locationData = $this->locationModel->getDataToCreateTable();
         $vehiclesData = $this->vehicleClassModel->getDataToCreateTable();
+        $ratesData = $this->activeRates->getDataToCreateTable();
         $brandTable = $this->db->createTable($brandData['table_name'], $brandData['table_columns']);
         $locationTable = $this->db->createTable($locationData['table_name'], $locationData['table_columns']);
         $vehicleTable = $this->db->createTable($vehiclesData['table_name'], $vehiclesData['table_columns']);
+        $rateTables = $this->db->createTable($ratesData['table_name'], $ratesData['table_columns']);
+        //this should be implemented on updates
         //$vehicleTableCheckUp = $this->db->updateTableOnChanges($vehiclesData['table_name'], $vehiclesData['table_columns']);
-        //$rateTables = $this->db->createTable()
         // add validation for process completed
     }
     public function createColumnsForVehiclesClassesCustomFields($customFields)

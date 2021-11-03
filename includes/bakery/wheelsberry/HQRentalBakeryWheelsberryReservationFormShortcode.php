2bade705b31e3e5a7ba1c4ffbe1b56e0c6bbefad
Miguel Faggioni <miguelfaggioni@gmail.com>
2021-11-03 13:24:34 -0400

    fixes for responsive

--- includes/bakery/wheelsberry/HQRentalBakeryWheelsberryReservationFormShortcode.php
+++ includes/bakery/wheelsberry/HQRentalBakeryWheelsberryReservationFormShortcode.php
@@ -132,6 +132,16 @@ class HQRentalBakeryWheelsberryReservationFormShortcode extends WPBakeryShortCod
                 .hq-wheelsberry-image-wrapper{
                     width: 45%;
                 }
+                @media only screen and (max-width: 519px){
+                    .hq-wheelsberry-image-wrapper{
+                       width: 100%;
+                    }
+                }
+                .hq-reservation-item-inner-wrapper{
+                    overflow:hidden;
+                }
+
+            }
             </style>
             <div id='hq-wheelsberry-slider' class='cars-slider' id='cars-slider'>
                 <div class='car-slider__title-wrapper om-container'>
@@ -156,7 +166,7 @@ class HQRentalBakeryWheelsberryReservationFormShortcode extends WPBakeryShortCod
                                 </div>
                             <form action='/reservations/' method='get'>
                                 <div class='reservation-form__line reservation-form__car'>
-                                    <div class='reservation-form__field-inner'>
+                                    <div class='reservation-form__field-inner hq-reservation-item-inner-wrapper'>
                                         <select class='reservation-form__car-select' id='reservation-form__car-select' name='vehicle_class_id'>
                                             <option></option>
                                             {$this->resolveOptionsForClasses($vehicle_classes)}
@@ -167,7 +177,7 @@ class HQRentalBakeryWheelsberryReservationFormShortcode extends WPBakeryShortCod
                                 <div class='reservation-form__more'>
                                     <div class='reservation-form__line reservation-form__set reservation-form__pick-up'>
                                         <div class='reservation-form__pick-up-location reservation-form__location'>
-                                            <div class='reservation-form__field-inner'>
+                                            <div class='reservation-form__field-inner hq-reservation-item-inner-wrapper'>
                                                 <label for='reservation-form__pick-up-location-select' class='reservation-form__label reservation-form__pick-up-location-label reservation-form__location-label'>Pick-up</label>
                                                 <select id='hq-pick-up-location' name='pick_up_location' class='reservation-form__pick-up-time-select' data-placeholder='Choose a location'>
                                                     <option>Select Location</option>
@@ -176,7 +186,7 @@ class HQRentalBakeryWheelsberryReservationFormShortcode extends WPBakeryShortCod
                                             </div>
                                         </div>
                                         <div class='reservation-form__pick-up-date reservation-form__date'>
-                                            <div class='reservation-form__field-inner'>
+                                            <div class='reservation-form__field-inner hq-reservation-item-inner-wrapper'>
                                                 <label for='reservation-form__pick-up-date-input' class='reservation-form__label reservation-form__pick-up-date-label reservation-form__date-label'>Pick-up</label>
                                                 <div class='reservation-form__date-wrapper'>
                                                     <input type='text' name='pick_up_date' readonly='readonly' placeholder='Choose a date' class='reservation-form__pick-up-date-input' id='reservation-form__pick-up-date-input' data-date-format='MM dd, yy' />
@@ -184,7 +194,7 @@ class HQRentalBakeryWheelsberryReservationFormShortcode extends WPBakeryShortCod
                                             </div>
                                         </div>
                                         <div class='reservation-form__pick-up-time reservation-form__time'>
-                                            <div class='reservation-form__field-inner'>
+                                            <div class='reservation-form__field-inner hq-reservation-item-inner-wrapper'>
                                                 <select name='pick_up_time' class='reservation-form__pick-up-time-select' id='reservation-form__pick-up-time-select'>
                                                     <option value='12:00 am'>12:00 am</option><option value='12:15 am'>12:15 am</option><option value='12:30 am'>12:30 am</option><option value='12:45 am'>12:45 am</option><option value='1:00 am'>1:00 am</option><option value='1:15 am'>1:15 am</option><option value='1:30 am'>1:30 am</option><option value='1:45 am'>1:45 am</option><option value='2:00 am'>2:00 am</option><option value='2:15 am'>2:15 am</option><option value='2:30 am'>2:30 am</option><option value='2:45 am'>2:45 am</option><option value='3:00 am'>3:00 am</option><option value='3:15 am'>3:15 am</option><option value='3:30 am'>3:30 am</option><option value='3:45 am'>3:45 am</option><option value='4:00 am'>4:00 am</option><option value='4:15 am'>4:15 am</option><option value='4:30 am'>4:30 am</option><option value='4:45 am'>4:45 am</option><option value='5:00 am'>5:00 am</option><option value='5:15 am'>5:15 am</option><option value='5:30 am'>5:30 am</option><option value='5:45 am'>5:45 am</option><option value='6:00 am'>6:00 am</option><option value='6:15 am'>6:15 am</option><option value='6:30 am'>6:30 am</option><option value='6:45 am'>6:45 am</option><option value='7:00 am'>7:00 am</option><option value='7:15 am'>7:15 am</option><option value='7:30 am'>7:30 am</option><option value='7:45 am'>7:45 am</option><option value='8:00 am'>8:00 am</option><option value='8:15 am'>8:15 am</option><option value='8:30 am'>8:30 am</option><option value='8:45 am'>8:45 am</option><option value='9:00 am'>9:00 am</option><option value='9:15 am'>9:15 am</option><option value='9:30 am'>9:30 am</option><option value='9:45 am'>9:45 am</option><option value='10:00 am'>10:00 am</option><option value='10:15 am'>10:15 am</option><option value='10:30 am'>10:30 am</option><option value='10:45 am'>10:45 am</option><option value='11:00 am'>11:00 am</option><option value='11:15 am'>11:15 am</option><option value='11:30 am'>11:30 am</option><option value='11:45 am'>11:45 am</option><option value='12:00 pm' selected='selected'>12:00 pm</option><option value='12:15 pm'>12:15 pm</option><option value='12:30 pm'>12:30 pm</option><option value='12:45 pm'>12:45 pm</option><option value='1:00 pm'>1:00 pm</option><option value='1:15 pm'>1:15 pm</option><option value='1:30 pm'>1:30 pm</option><option value='1:45 pm'>1:45 pm</option><option value='2:00 pm'>2:00 pm</option><option value='2:15 pm'>2:15 pm</option><option value='2:30 pm'>2:30 pm</option><option value='2:45 pm'>2:45 pm</option><option value='3:00 pm'>3:00 pm</option><option value='3:15 pm'>3:15 pm</option><option value='3:30 pm'>3:30 pm</option><option value='3:45 pm'>3:45 pm</option><option value='4:00 pm'>4:00 pm</option><option value='4:15 pm'>4:15 pm</option><option value='4:30 pm'>4:30 pm</option><option value='4:45 pm'>4:45 pm</option><option value='5:00 pm'>5:00 pm</option><option value='5:15 pm'>5:15 pm</option><option value='5:30 pm'>5:30 pm</option><option value='5:45 pm'>5:45 pm</option><option value='6:00 pm'>6:00 pm</option><option value='6:15 pm'>6:15 pm</option><option value='6:30 pm'>6:30 pm</option><option value='6:45 pm'>6:45 pm</option><option value='7:00 pm'>7:00 pm</option><option value='7:15 pm'>7:15 pm</option><option value='7:30 pm'>7:30 pm</option><option value='7:45 pm'>7:45 pm</option><option value='8:00 pm'>8:00 pm</option><option value='8:15 pm'>8:15 pm</option><option value='8:30 pm'>8:30 pm</option><option value='8:45 pm'>8:45 pm</option><option value='9:00 pm'>9:00 pm</option><option value='9:15 pm'>9:15 pm</option><option value='9:30 pm'>9:30 pm</option><option value='9:45 pm'>9:45 pm</option><option value='10:00 pm'>10:00 pm</option><option value='10:15 pm'>10:15 pm</option><option value='10:30 pm'>10:30 pm</option><option value='10:45 pm'>10:45 pm</option><option value='11:00 pm'>11:00 pm</option><option value='11:15 pm'>11:15 pm</option><option value='11:30 pm'>11:30 pm</option><option value='11:45 pm'>11:45 pm</option>
                                                 </select>
@@ -193,7 +203,7 @@ class HQRentalBakeryWheelsberryReservationFormShortcode extends WPBakeryShortCod
                                     </div>
                                     <div class='reservation-form__line reservation-form__set reservation-form__drop-off'>
                                         <div class='reservation-form__drop-off-location reservation-form__location'>
-                                            <div class='reservation-form__field-inner'>
+                                            <div class='reservation-form__field-inner hq-reservation-item-inner-wrapper'>
                                                 <label for='reservation-form__pick-up-location-select' class='reservation-form__label reservation-form__pick-up-location-label reservation-form__location-label'>Drop-off</label>
                                                 <select id='hq-return-location' name='return_location' class='reservation-form__pick-up-time-select' data-placeholder='Choose a location'>
                                                     <option>Select Location</option>
@@ -202,7 +212,7 @@ class HQRentalBakeryWheelsberryReservationFormShortcode extends WPBakeryShortCod
                                             </div>
                                         </div>
                                         <div class='reservation-form__drop-off-date reservation-form__date'>
-                                            <div class='reservation-form__field-inner'>
+                                            <div class='reservation-form__field-inner hq-reservation-item-inner-wrapper'>
                                                 <label for='reservation-form__drop-off-date-input' class='reservation-form__label reservation-form__drop-off-date-label reservation-form__date-label'>Drop-off</label>
                                                 <div class='reservation-form__date-wrapper'>
                                                     <input type='text' name='return_date' readonly='readonly' placeholder='Choose a date' class='reservation-form__drop-off-date-input' id='reservation-form__drop-off-date-input' data-date-format='MM dd, yy' />
@@ -210,7 +220,7 @@ class HQRentalBakeryWheelsberryReservationFormShortcode extends WPBakeryShortCod
                                             </div>
                                         </div>
                                         <div class='reservation-form__drop-off-time reservation-form__time'>
-                                            <div class='reservation-form__field-inner'>
+                                            <div class='reservation-form__field-inner hq-reservation-item-inner-wrapper'>
                                                 <select name='return_time' class='reservation-form__drop-off-time-select' id='reservation-form__drop-off-time-select'>
 												    <option value='12:00 am'>12:00 am</option><option value='12:15 am'>12:15 am</option><option value='12:30 am'>12:30 am</option><option value='12:45 am'>12:45 am</option><option value='1:00 am'>1:00 am</option><option value='1:15 am'>1:15 am</option><option value='1:30 am'>1:30 am</option><option value='1:45 am'>1:45 am</option><option value='2:00 am'>2:00 am</option><option value='2:15 am'>2:15 am</option><option value='2:30 am'>2:30 am</option><option value='2:45 am'>2:45 am</option><option value='3:00 am'>3:00 am</option><option value='3:15 am'>3:15 am</option><option value='3:30 am'>3:30 am</option><option value='3:45 am'>3:45 am</option><option value='4:00 am'>4:00 am</option><option value='4:15 am'>4:15 am</option><option value='4:30 am'>4:30 am</option><option value='4:45 am'>4:45 am</option><option value='5:00 am'>5:00 am</option><option value='5:15 am'>5:15 am</option><option value='5:30 am'>5:30 am</option><option value='5:45 am'>5:45 am</option><option value='6:00 am'>6:00 am</option><option value='6:15 am'>6:15 am</option><option value='6:30 am'>6:30 am</option><option value='6:45 am'>6:45 am</option><option value='7:00 am'>7:00 am</option><option value='7:15 am'>7:15 am</option><option value='7:30 am'>7:30 am</option><option value='7:45 am'>7:45 am</option><option value='8:00 am'>8:00 am</option><option value='8:15 am'>8:15 am</option><option value='8:30 am'>8:30 am</option><option value='8:45 am'>8:45 am</option><option value='9:00 am'>9:00 am</option><option value='9:15 am'>9:15 am</option><option value='9:30 am'>9:30 am</option><option value='9:45 am'>9:45 am</option><option value='10:00 am'>10:00 am</option><option value='10:15 am'>10:15 am</option><option value='10:30 am'>10:30 am</option><option value='10:45 am'>10:45 am</option><option value='11:00 am'>11:00 am</option><option value='11:15 am'>11:15 am</option><option value='11:30 am'>11:30 am</option><option value='11:45 am'>11:45 am</option><option value='12:00 pm' selected='selected'>12:00 pm</option><option value='12:15 pm'>12:15 pm</option><option value='12:30 pm'>12:30 pm</option><option value='12:45 pm'>12:45 pm</option><option value='1:00 pm'>1:00 pm</option><option value='1:15 pm'>1:15 pm</option><option value='1:30 pm'>1:30 pm</option><option value='1:45 pm'>1:45 pm</option><option value='2:00 pm'>2:00 pm</option><option value='2:15 pm'>2:15 pm</option><option value='2:30 pm'>2:30 pm</option><option value='2:45 pm'>2:45 pm</option><option value='3:00 pm'>3:00 pm</option><option value='3:15 pm'>3:15 pm</option><option value='3:30 pm'>3:30 pm</option><option value='3:45 pm'>3:45 pm</option><option value='4:00 pm'>4:00 pm</option><option value='4:15 pm'>4:15 pm</option><option value='4:30 pm'>4:30 pm</option><option value='4:45 pm'>4:45 pm</option><option value='5:00 pm'>5:00 pm</option><option value='5:15 pm'>5:15 pm</option><option value='5:30 pm'>5:30 pm</option><option value='5:45 pm'>5:45 pm</option><option value='6:00 pm'>6:00 pm</option><option value='6:15 pm'>6:15 pm</option><option value='6:30 pm'>6:30 pm</option><option value='6:45 pm'>6:45 pm</option><option value='7:00 pm'>7:00 pm</option><option value='7:15 pm'>7:15 pm</option><option value='7:30 pm'>7:30 pm</option><option value='7:45 pm'>7:45 pm</option><option value='8:00 pm'>8:00 pm</option><option value='8:15 pm'>8:15 pm</option><option value='8:30 pm'>8:30 pm</option><option value='8:45 pm'>8:45 pm</option><option value='9:00 pm'>9:00 pm</option><option value='9:15 pm'>9:15 pm</option><option value='9:30 pm'>9:30 pm</option><option value='9:45 pm'>9:45 pm</option><option value='10:00 pm'>10:00 pm</option><option value='10:15 pm'>10:15 pm</option><option value='10:30 pm'>10:30 pm</option><option value='10:45 pm'>10:45 pm</option><option value='11:00 pm'>11:00 pm</option><option value='11:15 pm'>11:15 pm</option><option value='11:30 pm'>11:30 pm</option><option value='11:45 pm'>11:45 pm</option>
                                                 </select>                                                
@@ -218,12 +228,12 @@ class HQRentalBakeryWheelsberryReservationFormShortcode extends WPBakeryShortCod
                                         </div>
                                     </div>
                                     <div class='reservation-form__line reservation-form__required-notice'>
-                                        <div class='reservation-form__field-inner'>
+                                        <div class='reservation-form__field-inner hq-reservation-item-inner-wrapper'>
                                             <div class='reservation-form__required-notice-box'>Please fill in all required fields</div>
                                         </div>
                                     </div>
                                     <div class='reservation-form__line reservation-form__submit'>
-                                        <div class='reservation-form__field-inner'>
+                                        <div class='reservation-form__field-inner hq-reservation-item-inner-wrapper'>
                                             <input type='hidden' name='target_step' value='3' />
                                             <input type='submit' class='reservation-form__submit-button' id='reservation-form__submit-button' value='Continue booking' />
                                             <circle class='path' cx='24' cy='24' r='20' fill='none' stroke='#fff' stroke-width='4'>

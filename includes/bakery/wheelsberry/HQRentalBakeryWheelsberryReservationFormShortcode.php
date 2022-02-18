61d2aaac73a4d6582708dc1fbef38edbe0c24f97
Miguel Faggioni <miguelfaggioni@gmail.com>
2022-02-18 09:33:29 -0400

    fix on styles

--- includes/bakery/wheelsberry/HQRentalBakeryWheelsberryReservationFormShortcode.php
+++ includes/bakery/wheelsberry/HQRentalBakeryWheelsberryReservationFormShortcode.php
@@ -174,6 +174,7 @@ class HQRentalBakeryWheelsberryReservationFormShortcode extends WPBakeryShortCod
                 }
                 .hq-upper-tag{
                     text-transform: uppercase;
+                    font-size: 16px;
                 }
             </style>
             <div id='hq-wheelsberry-slider' class='cars-slider' id='cars-slider'>
@@ -309,6 +310,7 @@ class HQRentalBakeryWheelsberryReservationFormShortcode extends WPBakeryShortCod
         if (is_array($vehicles) and count($vehicles)) {
             $html = '';
             foreach ($vehicles as $vehicle) {
+                dd($vehicle);
                 $priceHTML = "";
                 $priceHTML = !empty($vehicle->getActiveRate()->daily_rate->amount_for_display) ? ("<span class='cars-slider__item-price hq-upper-tag'>as low as</span><span class='omcr-price-currency hq-wheelsberry-daily-tag'>{$vehicle->getActiveRate()->daily_rate->amount_for_display} daily</span>") : "";
                 $priceHTML .= !empty($vehicle->getActiveRate()->daily_rate->amount_for_display) ? ("<span class='omcr-price-currency hq-wheelsberry-separator'> |</span> <span class='omcr-price-currency hq-wheelsberry-weekly-tag'>{$vehicle->getActiveRate()->weekly_rate->amount_for_display} weekly</span>") : "";

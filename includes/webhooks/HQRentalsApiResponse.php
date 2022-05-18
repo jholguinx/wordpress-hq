13e3709e85c0e31ef13766ca2fe3c49f1f0ca2b9
Miguel Faggioni <miguelfaggioni@gmail.com>
2022-05-18 13:27:26 -0400

    api respose

---
+++ includes/webhooks/HQRentalsApiResponse.php
@@ -0,0 +1,24 @@
+<?php
+namespace HQRentalsPlugin\HQRentalsWebhooks;
+
+
+class HQRentalsApiResponse{
+    protected $status;
+    protected $data;
+    protected $errors;
+
+    static function resolveSuccess($data) : array{
+        return [
+            'status' => 200,
+            'data' => $data,
+            'errors' => null
+        ];
+    }
+    static function resolveError($errors) : array{
+        return [
+            'status' => 500,
+            'data' => null,
+            'errors' => $errors
+        ];
+    }
+}

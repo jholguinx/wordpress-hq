c0f533c97686d3be9ab31c8e2d4845f72ed23f21
Miguel Faggioni <miguelfaggioni@gmail.com>
2022-07-27 09:20:17 -0400

    remove offset

--- includes/assets/js/hq-carousel.js
+++ includes/assets/js/hq-carousel.js
@@ -46,15 +46,10 @@ jQuery(document).ready(function(){
     if(form.length){
         var scroll = new SmoothScroll('a[href*="#"]',{
             speed: 1000,
+            /*
             offset: function (anchor, toggle) {
-
-                // Integer or Function returning an integer. How far to offset the scrolling anchor location in pixels
-                // This example is a function, but you could do something as simple as `offset: 25`
-
-                // An example returning different values based on whether the clicked link was in the header nav or not
                 return -350;
-
-            },
+            },*/
         });
     }
 });

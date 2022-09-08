76bb9f001a73a3abb44e9cc8369666eed784bb6f
Miguel Faggioni <miguelfaggioni@gmail.com>
2022-09-08 13:46:58 -0400

    base grid filter

--- includes/assets/js/hq-tabs.js
+++ includes/assets/js/hq-tabs.js
@@ -1,7 +1,6 @@
 jQuery(document).ready(function (){
     jQuery('#hq-tabs').tabs();
     jQuery('.trigger').on('click', function(e){
-        console.log(e);
         e.preventDefault();
     })
 });

1752863a2e06b9c305d3b6e9a1ee6ae597e17840
Miguel Faggioni <miguelfaggioni@gmail.com>
2022-09-12 12:46:51 -0400

    tabs on smooth scroll

--- includes/assets/js/hq-tabs.js
+++ includes/assets/js/hq-tabs.js
@@ -3,4 +3,14 @@ jQuery(document).ready(function (){
     jQuery('.trigger').on('click', function(e){
         e.preventDefault();
     })
+    /*smooth scroll disabled*/
+    jQuery( window ).on( 'elementor/frontend/init', function() {
+        if ( typeof elementorFrontend === 'undefined' ) {
+            return;
+        }
+
+        elementorFrontend.on( 'components:init', function() {
+            elementorFrontend.utils.anchors.setSettings( 'selectors.targets', '.dummy-selector' );
+        } );
+    } );
 });

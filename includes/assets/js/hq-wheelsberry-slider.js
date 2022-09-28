0cfc50bfea2813b05b2dc78ade030f73b43ed45d
Miguel Faggioni <miguelfaggioni@gmail.com>
2022-09-28 09:28:06 -0400

    adding changes on owl carousel due to changes on selected vehicle class

--- includes/assets/js/hq-wheelsberry-slider.js
+++ includes/assets/js/hq-wheelsberry-slider.js
@@ -4,5 +4,11 @@ jQuery(function(){
         var selectedVehicleClassId = jQuery('#reservation-form__car-select').val();
         var selectedVehicleSlide = jQuery('#hq-vehicle-wheelsberry-' + selectedVehicleClassId);
         owl.trigger('to.owl.carousel',[selectedVehicleSlide.attr('data-vehicle-class-index'),250]);
+        refreshAutoplayTimeout();
     });
 })
+function refreshAutoplayTimeout(){
+    var owl = jQuery('.owl-carousel');
+    owl.data('owl.carousel').options.autoplayTimeout = 10000;
+    owl.trigger('refresh.owl.carousel');
+}

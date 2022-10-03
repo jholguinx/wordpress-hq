01151b958df5e6cf8d27ec0aa2861d45a25f9e70
Miguel Faggioni <miguelfaggioni@gmail.com>
2022-10-03 08:41:02 -0400

    fix on selector

--- includes/assets/js/hq-wheelsberry-slider.js
+++ includes/assets/js/hq-wheelsberry-slider.js
@@ -1,14 +1,21 @@
+var carouselSelector = '#hq-carousel'
 jQuery(function(){
     jQuery('#reservation-form__car-select').on('change',function(){
-        var owl = jQuery('.owl-carousel');
+        var owl = jQuery(carouselSelector);
+        owl.owlCarousel();
         var selectedVehicleClassId = jQuery('#reservation-form__car-select').val();
         var selectedVehicleSlide = jQuery('#hq-vehicle-wheelsberry-' + selectedVehicleClassId);
-        owl.trigger('to.owl.carousel',[selectedVehicleSlide.attr('data-vehicle-class-index'),250]);
+        owl.trigger('to.owl.carousel',[ parseInt(selectedVehicleSlide.attr('data-vehicle-class-index')) ,250, true]);
         refreshAutoplayTimeout();
     });
-})
+});
 function refreshAutoplayTimeout(){
-    var owl = jQuery('.owl-carousel');
-    owl.data('owl.carousel').options.autoplayTimeout = 10000;
-    owl.trigger('refresh.owl.carousel');
+    try {
+        var owl = jQuery(carouselSelector);
+        owl.data('owlCarousel').options.autoplayTimeout = 10000;
+        owl.trigger('refresh.owl.carousel');
+    }catch (e) {
+        // do nothing
+    }
+
 }
